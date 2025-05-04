4442f43d1f8694e09e37dc038908a978f3eb8627
Dodo <dodo@rewohub.com>
2025-05-04 19:36:39 +0200

    add gpt2 input encoding

--- src/tokenizers/ByteLeveLBytePairEncodingTokenizer.zig
+++ src/tokenizers/ByteLeveLBytePairEncodingTokenizer.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
+const encoder = @import("./../utils/byte_to_unicode_gpt2.zig").create_bytes_to_unicode_map_gpt2;
 
 pub const Tokenizer = struct {
     vocab_path: []const u8,
     merges_path: []const u8,
     vocab: std.StringHashMap(u32),
+    inv_vocab: std.AutoHashMap(u32, []const u8), // shared memory with vocab
     merges: std.StringHashMap(std.StringHashMap(usize)),
 
     pub fn init(allocator: std.mem.Allocator, vocab_path: []const u8, merges_path: []const u8) !*Tokenizer {
@@ -12,34 +14,37 @@ pub const Tokenizer = struct {
         std.mem.copyForwards(u8, _vocab_path, vocab_path);
         const _merges_path = try allocator.alloc(u8, merges_path.len);
         std.mem.copyForwards(u8, _merges_path, merges_path);
-        tokenizer_ptr.* = .{ .vocab_path = _vocab_path, .merges_path = _merges_path, .vocab = std.StringHashMap(u32).init(allocator), .merges = std.StringHashMap(std.StringHashMap(usize)).init(allocator) };
+        tokenizer_ptr.* = .{ .vocab_path = _vocab_path, .merges_path = _merges_path, .vocab = std.StringHashMap(u32).init(allocator), .merges = std.StringHashMap(std.StringHashMap(usize)).init(allocator), .inv_vocab = std.AutoHashMap(u32, []const u8).init(allocator) };
 
         return tokenizer_ptr;
     }
     pub fn destroy(self: *Tokenizer, allocator: std.mem.Allocator) void {
-        std.debug.print("Start Deinit Tokenizer\n", .{});
+        std.debug.print("DESTROY 0\n", .{});
         allocator.free(self.*.vocab_path);
         allocator.free(self.*.merges_path);
 
-        std.debug.print("Deinit vocab\n", .{});
+        std.debug.print("DESTROY 1\n", .{});
+        var vocab_iter = self.*.vocab.iterator();
+        while (vocab_iter.next()) |entry| {
+            allocator.free(entry.key_ptr.*);
+        }
+        std.debug.print("DESTROY 2\n", .{});
         self.*.vocab.deinit();
-        std.debug.print("Finished deinit vocab\n", .{});
+        self.*.inv_vocab.deinit();
+        std.debug.print("DESTROY 3\n", .{});
 
-        std.debug.print("Deinit merges\n", .{});
         var it = self.merges.iterator();
         while (it.next()) |entry| {
             entry.value_ptr.*.deinit();
         }
 
+        std.debug.print("DESTROY 4\n", .{});
         self.*.merges.deinit();
-        std.debug.print("Finished deinit merges\n", .{});
         allocator.destroy(self);
-
-        std.debug.print("Finished deinit Tokenizer\n", .{});
+        std.debug.print("DESTROY 5\n", .{});
     }
 
     fn loadVocab(self: *Tokenizer, allocator: std.mem.Allocator) !void {
-        std.debug.print("Start loadVocab\n", .{});
         const TEN_MEGABYTE = 10 * 1024 * 1024;
         const vocab_bytes = try std.fs.cwd().readFileAlloc(allocator, self.*.vocab_path, TEN_MEGABYTE);
         defer allocator.free(vocab_bytes);
@@ -73,6 +78,7 @@ pub const Tokenizer = struct {
             // Duplicate key into allocator memory
             const dup = try allocator.dupe(u8, key);
             try self.*.vocab.put(dup, id);
+            try self.*.inv_vocab.put(id, dup);
         }
 
         std.debug.print("Finished loadVocab\n", .{});
@@ -85,26 +91,42 @@ pub const Tokenizer = struct {
         var line_buf: [1024]u8 = undefined;
         var prio: usize = 0;
         while (true) {
-            const line = try reader.readUntilDelimiter(line_buf[0..], '\n');
-            if (line.len == 0) break;
+            std.debug.print("MERGES 0 \n", .{});
+            const maybe_line = try reader.readUntilDelimiterOrEof(line_buf[0..], '\n');
+            std.debug.print("MERGES 1 {any}\n", .{maybe_line});
+
+            if (maybe_line == null) {
+                std.debug.print("MERGES 40 \n", .{});
+                break;
+            }
+            const line = maybe_line.?;
+            std.debug.print("MERGES 2 {s}\n", .{line});
             const trimmed = std.mem.trim(u8, line, " \t\r\n");
             if (trimmed.len == 0 or std.mem.startsWith(u8, trimmed, "#")) continue;
             const idx = std.mem.indexOf(u8, trimmed, " ") orelse continue;
+            std.debug.print("MERGES 10 \n", .{});
             const a = trimmed[0..idx];
             const b = std.mem.trim(u8, trimmed[idx + 1 ..], " \t\r\n");
+            std.debug.print("MERGES 20 \n", .{});
             // Insert into the two-level map: merges[a][b] = priority
-            if (self.merges.get(a)) |inner| {
+            if (self.*.merges.getPtr(a)) |inner| {
+                std.debug.print("MERGES 21 \n", .{});
                 const bdup = try allocator.dupe(u8, b);
-                try inner.put(bdup, prio);
+                try inner.*.put(bdup, prio);
+                std.debug.print("MERGES 24 \n", .{});
             } else {
+                std.debug.print("MERGES 25 \n", .{});
                 var newInner = std.StringHashMap(usize).init(allocator);
                 const adup = try allocator.dupe(u8, a);
                 const bdup = try allocator.dupe(u8, b);
                 try newInner.put(bdup, prio);
-                try self.merges.put(adup, newInner);
+                try self.*.merges.put(adup, newInner);
+                std.debug.print("MERGES 27 \n", .{});
             }
             prio += 1;
+            std.debug.print("MERGES 29 \n", .{});
         }
+        std.debug.print("MERGES 30 \n", .{});
     }
 
     pub fn tokenize(
@@ -116,30 +138,51 @@ pub const Tokenizer = struct {
         var arena = std.heap.ArenaAllocator.init(allocator);
         const aalloc = arena.allocator();
 
-        //const normalized_input = try aalloc.alloc(u8, input.len);
+        if (self.*.vocab.count() == 0) {
+            self.*.loadVocab(allocator) catch {
+                _ = try std.io.getStdErr().write("Unable to load vocab.json file ");
+                _ = try std.io.getStdErr().write(self.*.vocab_path);
+                _ = try std.io.getStdErr().write(".\n");
+            };
+        }
+        if (self.*.merges.count() == 0) {
+            self.*.loadMerges(allocator) catch {
+                _ = try std.io.getStdErr().write("Unable to load merges.txt file ");
+                _ = try std.io.getStdErr().write(self.*.merges_path);
+                _ = try std.io.getStdErr().write(".\n");
+            };
+        }
 
-        //for (input, 1..input.len + 1) |char, index| {
-        //    if (char == ' ') {
-        //        normalized_input[index + 1] = 'Ġ';
-        //    } else {
-        //        normalized_input[index + 1] = char;
-        //    }
-        //}
+        // normalize input data
+        var normalized_input_list = std.ArrayList(u8).init(allocator);
+        defer normalized_input_list.deinit();
+        const normalizer_ptr = try encoder(allocator);
+        defer {
+            for (normalizer_ptr.*) |entry| {
+                allocator.free(entry);
+            }
+            allocator.destroy(normalizer_ptr);
+        }
 
+        for (input) |char| {
+            const codepoint = normalizer_ptr.*[char];
+            for (codepoint) |byte| {
+                try normalized_input_list.append(byte);
+            }
+        }
+
+        const normalized_input = normalized_input_list.items;
         // Prepend space to input.
-        const prefixed_len = input.len + 1;
-        const prefixed = try aalloc.alloc(u8, prefixed_len);
-        prefixed[0] = 0x20;
-        std.mem.copyForwards(u8, prefixed[1..], input);
 
+        std.debug.print("[DODO] Normalized input {s}\n", .{normalized_input});
         // Build initial one-byte parts.
         var parts = std.ArrayList([]u8).init(aalloc);
-        for (prefixed) |c| {
+        for (normalized_input) |c| {
             const slice = try aalloc.alloc(u8, 1);
             slice[0] = c;
             try parts.append(slice);
         }
-        std.debug.print("parts: {any}\n", .{parts.items});
+        std.debug.print("parts: {s}\n", .{parts.items});
 
         // BPE merge loop with O(1) lookup.
         var changed = true;
@@ -150,7 +193,8 @@ pub const Tokenizer = struct {
             for (0..parts.items.len - 1) |i| {
                 const x = parts.items[i];
                 const y = parts.items[i + 1];
-                if (self.merges.get(x)) |inner| {
+                std.debug.print("loop x {s} y {s}\n", .{ x, y });
+                if (self.*.merges.get(x)) |inner| {
                     if (inner.get(y)) |p| {
                         if (p < best_p) {
                             best_p = p;
@@ -185,50 +229,99 @@ pub const Tokenizer = struct {
         arena.deinit();
         return result;
     }
+
+    pub fn detokenize(self: *Tokenizer, allocator: std.mem.Allocator, input: []const u32) ![]const u8 {
+        std.debug.print("{any} {any} {any}", .{ self, allocator, input });
+
+        // load vocab & merges if not loaded yet.
+        if (self.*.vocab.count() == 0) {
+            self.*.loadVocab(allocator) catch {
+                _ = try std.io.getStdErr().write("Unable to load vocab.json file ");
+                _ = try std.io.getStdErr().write(self.*.vocab_path);
+                _ = try std.io.getStdErr().write(".\n");
+            };
+        }
+        if (self.*.merges.count() == 0) {
+            self.*.loadMerges(allocator) catch {
+                _ = try std.io.getStdErr().write("Unable to load merges.txt file ");
+                _ = try std.io.getStdErr().write(self.*.merges_path);
+                _ = try std.io.getStdErr().write(".\n");
+            };
+        }
+
+        var inv_vocab = std.AutoHashMap(u32, []const u8).init(allocator);
+
+        var vocab_iter = self.*.vocab.iterator();
+
+        while (vocab_iter.next()) |entry| {
+            try inv_vocab.put(entry.value_ptr.*, entry.key_ptr.*);
+        }
+
+        const some = "hello";
+        const out = try allocator.dupe(u8, some);
+        return out;
+    }
 };
 
-test "tokenize aaa b" {
+test "tokenize aaab" {
+    std.debug.print("DODO 1\n", .{});
     const given_allocator = std.testing.allocator;
 
     // 1) Prepare vocab and merges (you may load these from files or define inline for the test)
     // Here we assume you have helper functions that load them synchronously.
     //const given_vocab_path = "../../test_data/byte_level_byte_pair_encoding/vocab.json";
 
-    const given_tokenizer_ptr: *Tokenizer = try Tokenizer.init(given_allocator, "dummy_vocab_path.json", "dummy_merge_path.txt");
+    const given_tokenizer_ptr: *Tokenizer = try Tokenizer.init(given_allocator, "./test_data/mini_vocab.json", "./test_data/mini_merges.txt");
     defer given_tokenizer_ptr.*.destroy(given_allocator);
 
-    // Manually populate the vocabulary
-    try given_tokenizer_ptr.*.vocab.put("a", 0);
-    try given_tokenizer_ptr.*.vocab.put("b", 1);
-    try given_tokenizer_ptr.*.vocab.put("aa", 2);
-    try given_tokenizer_ptr.*.vocab.put("aaa", 3);
-
-    var inner_0 = std.StringHashMap(usize).init(given_allocator);
-    try inner_0.put("a", 0);
-    try given_tokenizer_ptr.*.merges.put("a", inner_0); // rule a -> a
-
-    var inner_1 = std.StringHashMap(usize).init(given_allocator);
-    try inner_1.put("aa", 1);
-    try given_tokenizer_ptr.*.merges.put("aa", inner_1);
-
+    std.debug.print("DODO 2\n", .{});
     // 2) The input string to test
-    const given_input = "aaa b";
+    const given_input = "aaab";
 
     // 3) The expected token ID sequence
     //    You need to run the real tokenizer once (e.g., in Python) to know these IDs.
     //    Here’s an example dummy sequence; replace with the real one:
     const expected_ids = [_]u32{ 3, 1 };
 
+    std.debug.print("DODO 3\n", .{});
     // 4) Tokenize and compare
     const actual_ids = try given_tokenizer_ptr.*.tokenize(given_allocator, given_input);
     defer given_allocator.free(actual_ids);
+    std.debug.print("DODO 4 {any}\n", .{actual_ids});
 
     // Assert same length
-    std.debug.print("expected:{any} actual:{any}\n", .{ expected_ids, actual_ids });
     try std.testing.expectEqual(actual_ids.len, expected_ids.len);
 
+    std.debug.print("DODO 5\n", .{});
     // Assert each ID matches
     for (actual_ids, 0..) |id, i| {
         try std.testing.expectEqual(id, expected_ids[i]);
     }
+
+    std.debug.print("DODO 6\n", .{});
+}
+
+test "detokenize" {
+    std.debug.print("Unitl here 0\n", .{});
+    const given_allocator = std.testing.allocator;
+
+    // 1) Prepare vocab and merges (you may load these from files or define inline for the test)
+    // Here we assume you have helper functions that load them synchronously.
+    //const given_vocab_path = "../../test_data/byte_level_byte_pair_encoding/vocab.json";
+
+    const given_tokenizer_ptr: *Tokenizer = try Tokenizer.init(given_allocator, "./test_data/mini_vocab.json", "./test_data/mini_merges.txt");
+    defer given_tokenizer_ptr.*.destroy(given_allocator);
+
+    // 3) The expected token ID sequence
+    //    You need to run the real tokenizer once (e.g., in Python) to know these IDs.
+    //    Here’s an example dummy sequence; replace with the real one:
+    const given_tokens = [_]u32{ 3, 1 };
+    const expected_output = "aaab";
+
+    // 4) Tokenize and compare
+    const actual_output = try given_tokenizer_ptr.*.detokenize(given_allocator, &given_tokens);
+    defer given_allocator.free(actual_output);
+
+    // Assert same length
+    try std.testing.expectEqual(expected_output, actual_output);
 }
